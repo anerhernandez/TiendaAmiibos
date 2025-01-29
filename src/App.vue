10d8d1f5f12afcb568f47ae31e7e4772c80c3bef
anerhernandez <anerhernandezl@gmail.com>
2025-01-29 16:07:37 +0000

    Added cart functionality (fix display bug)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, computed } from 'vue'
+import { jsx } from 'vue/jsx-runtime'
 
 let array = ref(null) // Aquí se almacenan todos los amiibos
 let currentPage = ref(1) // Página actual
 let itemsPerPage = 15 // Número de amiibos por página
 let personaje = '' // Filtro por nombre
 let url = "https://www.amiiboapi.com/api/amiibo/"
+let carrito = ref(null)
 
 // Función para obtener los datos de la API
 const api = async () => {
@@ -32,6 +34,33 @@ function limpiarfiltros() {
   currentPage.value = 1 // Reinicia la página al limpiar filtros
   api()
 }
+function mostrarcarrito(){
+  document.getElementById("carrito").setAttribute("style", "display:inline")
+  document.getElementById("bcarritO").setAttribute("style", "display:inline")
+  document.getElementById("bcarritoM").setAttribute("style", "display:none")
+}
+function ocultarcarrito(){
+  document.getElementById("carrito").setAttribute("style", "display:none")
+  document.getElementById("bcarritO").setAttribute("style", "display:none")
+  document.getElementById("bcarritoM").setAttribute("style", "display:inline")
+}
+function addtocart(amiibo){
+   alert("Se ha añadido al carrito: " + amiibo)
+   if (localStorage.getItem("amiibos") === null) {
+     localStorage.setItem("amiibos", JSON.stringify([amiibo]));
+     carrito = JSON.parse(localStorage.getItem("amiibos"));
+   }else{
+     carrito = JSON.parse(localStorage.getItem("amiibos"));
+     carrito.push(amiibo)
+     localStorage.setItem("amiibos", JSON.stringify(carrito))
+   }
+    carrito.forEach(element => {
+      let texto = document.createElement("p")
+      texto.textContent = element;
+      console.log(element)
+      document.getElementById("amiibos").appendChild(texto)
+    });
+}
 
 // Propiedad computada para obtener los amiibos de la página actual
 const paginatedArray = computed(() => {
@@ -48,7 +77,7 @@ const totalPages = computed(() => {
 </script>
 
 <template>
-  <div class="dark:bg-normal-950 dark:text-white">
+  <div class="dark:bg-neutral-800 dark:text-white">
     <div v-if="array != null">
       <!-- Filtros -->
       <header>
@@ -63,19 +92,23 @@ const totalPages = computed(() => {
           class="mx-4 p-2 border border-gray-300 rounded-lg">
           Limpiar filtros
         </button>
-        <button @click="limpiarfiltros" type="button" name="carrito" id="carrito"
+        <button @click="mostrarcarrito" type="button" name="bcarritoM" id="bcarritoM"
           class="mx-4 p-2 border border-gray-300 rounded-lg">
           Mostrar carrito
+        </button>
+        <button @click="ocultarcarrito" type="button" name="bcarritO" id="bcarritO"
+          class="mx-4 p-2 border border-gray-300 rounded-lg hidden">
+          Ocultar carrito
         </button>
       </header>
 
       <!-- No se ejecuta la muestra ni páginas si el amiibo buscado no existe -->
       <div v-if="array != ''">
         <!-- Listado de Amiibos -->
-        <div v-if="paginatedArray.length > 0" class="place-self-center">
+        <div v-if="paginatedArray.length > 0" class="place-self-center dark:bg-neutral-800">
           <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
             <div v-for="(item, index) in paginatedArray" :key="index"
-              class="rounded overflow-hidden shadow-lg place-self-center">
+              class="rounded overflow-hidden shadow-lg place-self-center dark:bg-neutral-900 w-96">
               <div class="flex justify-center">
                 <img class="w-36 md:w-52 lg:w-64" :src="item.image" />
               </div>
@@ -93,7 +126,7 @@ const totalPages = computed(() => {
                   <p class="text-gray-700 text-base p-1 dark:text-white">
                     Game Series: <b>{{ item.gameSeries }}</b>
                   </p>
-                  <button :value=item.name>Añadir a carrito</button>
+                  <button :value=item.name @click="addtocart(item.name)" class="dark:bg-neutral-800">Añadir a carrito</button>
                 </div>
               </div>
             </div>
@@ -101,7 +134,7 @@ const totalPages = computed(() => {
         </div>
 
         <!-- Controles de Paginación -->
-        <div class="flex justify-center mt-6">
+        <div class="flex justify-center mt-6 fixed bottom-0 left-0 right-0 bg-neutral-100">
           <button @click="currentPage--" :disabled="currentPage === 1"
             class="px-4 py-2 m-1 bg-gray-300 rounded hover:bg-gray-400 disabled:opacity-50">
             Anterior
